75b1f6eaf74646a4cc95b5ce4ace480e9a62f1af
Masaki Saito <masaki.saito.5u@stu.hosei.ac.jp>
2023-01-04 20:39:24 +0900

    changed follow's routing,  created follows and followers page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   put "follows/follow/:id" => "follows#follow", :as => 'follows_follow'
   put "follows/cancel/:id" => "follows#cancel", :as => 'follows_cancel'
-  resources :follows
+  # resources :follows
   devise_for :users
   get "tweets", :to => "tweets#index", :as => :user_root
   get "users/show"
 
-  resources :users
+  resources :users do
+    resources :follows
+  end
   resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
